cac62187a8473edb4f16ba2a10fe9b2572212bfd
Chatchanan-V <chatchanan.tae@gmail.com>
2022-06-03 14:08:03 +0100

    test: verified behavior when modifying an existing exchange rate

--- py/test_money.py
+++ py/test_money.py
@@ -67,12 +67,14 @@ class TestMoney(unittest.TestCase):
         tenEuros = Money(10, "EUR")
         self.assertEqual(self.bank.convert(tenEuros, "USD"), Money(12, "USD"))
 
-    def testConversionWithMissingExchangeRate(self):
+        self.bank.addExchangeRate("EUR", "USD", 1.3)
+        self.assertEqual(self.bank.convert(tenEuros, "USD"), Money(13, "USD"))
+
+    def testConversionWithDifferentRatesBetweenTwoCurrencies(self):
         bank = Bank()
         tenEuros = Money(10, "EUR")
         with self.assertRaisesRegex(Exception, "EUR->Kalganid"):
             bank.convert(tenEuros, "Kalganid")
-
 
 if __name__ == "__main__":
     unittest.main()
